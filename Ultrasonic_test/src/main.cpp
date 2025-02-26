8b438b22353f7baa76ac401b03d1ce83f3a0a644
ameerlouly <loulyameer@gmail.com>
2025-02-26 22:19:01 +0200

    Comments

--- Ultrasonic_test/src/main.cpp
+++ Ultrasonic_test/src/main.cpp
@@ -3,6 +3,12 @@
 
 
 void setup() {
+
+  /*ESP NOW Init Start*/
+
+
+  /*ESP NOW Init End*/
+
   Sensor_init(HCSR04);
   Serial.begin(9600);
 }
@@ -10,5 +16,9 @@ void setup() {
 void loop() {
   Serial.println(getDepth_cm(HCSR04));
   delay(500);
+
+  // ESP NOW Send Data
 }
+
+//todo: Create ESP Now Send Data Call Back Function
 
