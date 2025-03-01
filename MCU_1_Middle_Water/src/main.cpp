c47c7fadc602eb00c19f1c2345bba82390e70e5e
ameerlouly <loulyameer@gmail.com>
2025-03-02 00:00:31 +0200

    Adjusted ESPNOW Send Sktech

--- MCU_1_Middle_Water/src/main.cpp
+++ MCU_1_Middle_Water/src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include "Water_Level/Water_Level.h"
+#include "ESPNOW_Sender/Sender.h"
+
+SensorData data;
 
 
 void setup()
@@ -11,13 +14,19 @@ void setup()
   /*ESP NOW Init End*/
 
   Sensor_init(HCSR04);
+  ESPNOW_Sender_Init();
   Serial.begin(9600);
 }
 
 void loop()
 {
-  Serial.println(getDepth_Average_cm(HCSR04));
+  data.water_level = getDepth_Average_cm(HCSR04); // Measure Water Level
+  // Todo: Add measure temprature
+
+  Serial.println(data.water_level);
   delay(500);
+
+  ESPNOW_SendData(&data, sizeof(data));
 
   // ESP NOW Send Data
 }
