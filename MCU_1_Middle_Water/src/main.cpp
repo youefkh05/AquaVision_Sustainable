0b229cf1a61a28669fd206e456475ef446b4fbcb
ameerlouly <ameer.Louly04@eng-st.cu.edu.eg>
2025-03-24 10:03:12 +0200

    Turn Off FIrebase Temporarily

--- MCU_1_Middle_Water/src/main.cpp
+++ MCU_1_Middle_Water/src/main.cpp
@@ -19,8 +19,8 @@ void loop()
   data.water_level = getDepth_Average_cm(CurrentSensor); // Measure Water Level
   data.temperature = Temp_getTempC();
 
-  Serial.printf("Middle Water Level:  %f",data.water_level);
-  Serial.printf("Water Temprature:    %f",data.temperature);
+  Serial.printf("Middle Water Level:  %.02f\n",data.water_level);
+  Serial.printf("Water Temprature:    %.02f\n",data.temperature);
 
   ESPNOW_SendData(&data, sizeof(data));
   // ESP NOW Send Data
--- MCU_2_Water_Edge/src/main.cpp
+++ MCU_2_Water_Edge/src/main.cpp
@@ -55,7 +55,7 @@ void setup()
     /*Intializations*/
   // Sensor_init(current_sensor);
   OLED_init();
-  Setup_Firebase();
+  // Setup_Firebase();
   ESPNOW_Receiver_Init();
 
   Serial.begin(9600);
@@ -287,11 +287,11 @@ void loop()
 
   /* Send Data to web server **********************************************************************/
 
-  /* 🔹 Enable WiFi and send data to Firebase */
-  Enable_WiFi();
-  Send_Firebase_Data(AllData.water_level_1, AllData.temp, AllData.water_level_2, -200);
+  // /* 🔹 Enable WiFi and send data to Firebase */
+  // Enable_WiFi();
+  // Send_Firebase_Data(AllData.water_level_1, AllData.temp, AllData.water_level_2, -200);
 
-  /* 🔹 Switch back to ESP-NOW mode (Disconnect WiFi from router) */
-  Enable_ESPNow();
+  // /* 🔹 Switch back to ESP-NOW mode (Disconnect WiFi from router) */
+  // Enable_ESPNow();
 
 }
